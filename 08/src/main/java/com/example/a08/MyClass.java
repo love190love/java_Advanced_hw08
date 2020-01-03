2d018348b1b58c315a62e503a8402a3ca868a23a
love190love <love190love@yahoo.com.tw>
2020-01-03 14:38:32 +0800

    Initial commit

---
+++ 08/src/main/java/com/example/a08/MyClass.java
@@ -0,0 +1,12 @@
+package com.example.a08;
+
+
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        CppBank cppBank = new CppBank(Integer.parseInt(scanner.next()),Integer.parseInt(scanner.next()));
+    }
+}
+
